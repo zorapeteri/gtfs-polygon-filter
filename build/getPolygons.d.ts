40f86ed437d9629a1fadba309c2953c5c721c8ef
Zora Péteri <zorapeteri@gmail.com>
2023-07-01 01:16:50 +0200

    update gitignore

--- build/getPolygons.d.ts
+++
@@ -1,3 +0,0 @@
-import { Feature, Polygon } from 'geojson';
-import { GtfsPolygonFilterProps } from './index.js';
-export declare function getPolygons(polygons: GtfsPolygonFilterProps['polygon']): Feature<Polygon>[];
